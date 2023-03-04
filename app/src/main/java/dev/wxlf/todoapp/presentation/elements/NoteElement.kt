e18eec2a80078c786ec2b32e3eec71812d1feead
wxlf512 <wxlf512@gmail.com>
2023-03-05 00:48:50 +0300

    Added note sharing

--- app/src/main/java/dev/wxlf/todoapp/presentation/elements/NoteElement.kt
+++ app/src/main/java/dev/wxlf/todoapp/presentation/elements/NoteElement.kt
@@ -1,5 +1,7 @@
 package dev.wxlf.todoapp.presentation.elements
 
+import android.content.Intent
+import android.content.Intent.ACTION_SEND
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +9,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedCard
@@ -20,12 +23,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.os.bundleOf
 import dev.wxlf.todoapp.R
 import dev.wxlf.todoapp.data.entities.NoteEntity
 import java.time.LocalDateTime
@@ -43,6 +49,7 @@ fun NoteElement(
     delete: () -> Unit
 ) {
     var expanded by remember { mutableStateOf(false) }
+    val context = LocalContext.current
     ElevatedCard(modifier = modifier) {
         Column(modifier = Modifier.padding(8.dp)) {
             Row(Modifier.height(27.dp), verticalAlignment = Alignment.CenterVertically) {
@@ -70,6 +77,22 @@ fun NoteElement(
                             leadingIcon = {
                                 Icon(
                                     Icons.Default.Delete,
+                                    contentDescription = "Delete note"
+                                )
+                            }
+                        )
+                        DropdownMenuItem(text = { Text("Share it") },
+                            onClick = {
+                                expanded = false
+                                val intent = Intent(ACTION_SEND)
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                                intent.putExtra(Intent.EXTRA_TEXT, "${note.noteName}\n\n${note.noteData}")
+                                intent.type = "text/html"
+                                startActivity(context, Intent.createChooser(intent, "Share with Friends"), bundleOf())
+                            },
+                            leadingIcon = {
+                                Icon(
+                                    Icons.Default.Share,
                                     contentDescription = "Delete note"
                                 )
                             }
